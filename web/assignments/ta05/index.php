cf973e2361b1af14a8aeb0b4da905acd6c2ad774
Steven <smilton81@gmail.com>
2019-05-21 11:07:26 -0400

    corrected db connect issue

--- web/assignments/ta05/index.php
+++ web/assignments/ta05/index.php
@@ -25,7 +25,7 @@ switch ($action){
         include "view/list.php";
         break;
     case 'find':
-        //var_dump('Find');
+        var_dump('Find');
         //$searchscript = filter_input(INPUT_POST, 'searchscript', FILTER_SANITIZE_STRING);
         //var_dump($searchscript);
         //$scriptures = findScriptures($searchscript);
