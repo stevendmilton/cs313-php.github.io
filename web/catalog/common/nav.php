2d856a67b6c71757dbd7c66487955e69b4c71683
Steven <smilton81@gmail.com>
2019-05-25 17:00:44 -0400

    library project pages

--- web/catalog/common/nav.php
+++ web/catalog/common/nav.php
@@ -1,5 +1,5 @@
     <ul id="mainnav">
-        <li><a href="/web/catalog/index.php?action=">Library Search</a></li>
-        <li><a href="/web/catalog/index.php?action=authors">Author Maintenance</a></li>
-        <li><a href="/web/catalog/index.php?action=books">Book Maintenance</a></li>
+        <li><a href="index.php?action=">Library Search</a></li>
+        <li><a href="index.php?action=authors">Author Maintenance</a></li>
+        <li><a href="index.php?action=books">Book Maintenance</a></li>
     </ul>
