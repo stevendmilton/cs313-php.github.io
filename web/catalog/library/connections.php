fff4deb52b98000167e8a9e26f8fe0d993b00ca4
Steven <smilton81@gmail.com>
2019-06-01 15:58:41 -0400

    db access changes complete

--- web/catalog/library/connections.php
+++ web/catalog/library/connections.php
@@ -4,8 +4,52 @@
 * Database connections
 */
 
-
 function dbConnect(){
+  if (stripos($_SERVER["HTTP_HOST"], "localhost") === 0
+      || stripos($_SERVER["HTTP_HOST"], "127.0.0.1") === 0) {
+    // Running on local server
+    $server = "localhost";
+    $database = "catalog";
+    $user = "iClient";
+    $password = "l8n6kgKgpHjPbcue";
+    $dsn = 'mysql:host='.$server.';dbname='.$database;
+    $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+
+    try {
+        $acmeLink = new PDO($dsn, $user, $password, $options);
+        return $acmeLink;
+    } catch(PDOException $exc) {
+        header('location: view/500.php');
+        exit;
+    }
+  } else {
+    $db = NULL;
+    try
+    {
+      $dbUrl = getenv('DATABASE_URL');
+
+      $dbOpts = parse_url($dbUrl);
+
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
+
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $ex)
+    {
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+    }
+    return $db;
+  }
+}
+
+function abConnect(){
   $db = NULL;
   try
   {
@@ -32,7 +76,7 @@ function dbConnect(){
 }
 
 
-function abConnect(){
+function cbConnect(){
   if (stripos($_SERVER["HTTP_HOST"], "localhost") === 0
       || stripos($_SERVER["HTTP_HOST"], "127.0.0.1") === 0) {
     // Running on local server
